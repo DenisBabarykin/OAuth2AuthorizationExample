c2cb128554f3de73c27157f73b8fdd2127b03b8f
Denis Babarykin <babarykin.denis@gmail.com>
2016-10-19 02:40:47 +0300

    Access token recieving implemented

--- src/WebApp/src/WebApp/Controllers/VkAuthorizationController.cs
+++ src/WebApp/src/WebApp/Controllers/VkAuthorizationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SimpleWebRequests;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,6 +17,13 @@ namespace WebApp.Controllers
             if (!parameters.ContainsKey("code"))
                 return View("AuthorizationFail");
             string code = parameters["code"];
+            dynamic response = RESTRequest.PostAsUrlEncodedWithJsonResponse("https://oauth.vk.com/access_token", new Dictionary<string, string>()
+            {
+                { "client_id", "5671241"},
+                { "client_secret", "7nHh8NwJJeaZrW7juOyZ" },
+                { "redirect_uri", "http://localhost:56230/VkAuthorization" },
+                { "code", code }
+            });
             return View("AuthorizationSucces");
         }
     }
